24d9d4fac1d2b0b559bd3b79a907ae5e6c26bfac
pavankumar017 <pavankumarbr17@gmail.com>
2022-06-25 11:00:55 +0530

    firstcommit

---
+++ starter/script.js
@@ -0,0 +1,83 @@
+/*
+alert("js is fun ")
+
+console.log("jonas");
+
+let myCountry = "India";
+let myContinet = "Asia";
+let myCountryPopulation = 13000000
+
+console.log(myCountry)
+console.log(myContinet)
+console.log(myCountryPopulation)
+
+const currentYear = 2021;
+const myAge = currentYear- 1996 ;
+const mySisAge  = currentYear -1998;
+console.log(myAge , mySisAge);
+
+console.log("john" + "name");
+*/
+
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+Your tasks:
+1. StoreMark'sandJohn'smassandheightinvariables
+2. CalculateboththeirBMIsusingtheformula(youcanevenimplementboth
+versions)
+3. CreateaBooleanvariable'markHigherBMI'containinginformationabout
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+*/
+
+// const markHeight = 1.69 ;
+// const johnHeight = 1.88;
+// const markWeight = 78;
+// const johnWeight = 95 ;
+
+// let markBMI = markWeight / (markHeight * markHeight);
+// let jhonBMI = johnWeight / (johnHeight * johnHeight);
+
+// console.log("Marks BMI = " + markBMI);
+// console.log("jHONS BMI = " + jhonBMI);
+
+// markHigherBMI = markBMI > jhonBMI ;
+
+// console.log(markHigherBMI)
+
+// str1 = "Hi"
+// str2  =  "JS"
+// console.log(str1 + str2 );
+
+// const str3 = `im${str1}${str2}` ;//template literal
+// console.log(str3)
+
+
+// Calculate average score of the teams
+const dolphins1 = 23;
+const dolphins2 = 34;
+const dolphins3 = 89;
+
+const koalas1 = 88;
+const koalas2 = 3;
+const koalas3 = 7;
+
+avg_dolphins = dolphins1 + dolphins2 + dolphins3  /3
+console.log(avg_dolphins)
+
+avg_koalas = koalas1 + koalas2 + koalas3 /3
+console.log(avg_koalas)
+
+
+if (avg_dolphins > avg_koalas && avg_dolphins > 100) {
+    console.log("Dolphin is the winner");
+} else if (avg_koalas > avg_dolphins && avg_koalas > 100) {
+    console.log("koalas is the winner");
+}else console.log("no oneis winner")
+
+
+ 
